5f5a5f4136188cda653d57754d76e38a49481494
w1049 <w10493wang@163.com>
2022-08-08 10:10:15 +0800

    CORS

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.include_router(auth.router)
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['http://localhost:5173'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
