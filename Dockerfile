69f796605bbb5835a6141bb5e3232febbcd9ca72
w1049 <w10493wang@163.com>
2022-08-08 19:26:26 +0800

    docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9-slim
+
+COPY requirements.txt /tmp/requirements.txt
+RUN pip install --no-cache-dir -r /tmp/requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple && \
+pip install --no-cache-dir --no-dependencies pymilvus==1.1.2 -i https://mirror.sjtu.edu.cn/pypi/web/simple
+
+COPY ./app /app
---
+++ rocketqa/rocketqa_service.py
@@ -0,0 +1,74 @@
+# 修改自
+# https://github.com/PaddlePaddle/RocketQA/blob/main/examples/faiss_example/rocketqa_service.py
+import json
+import time
+
+import rocketqa
+from tornado import ioloop
+from tornado import web
+
+
+class RocketQAServer(web.RequestHandler):
+
+    def __init__(self, application, request, **kwargs):
+        web.RequestHandler.__init__(self, application, request)
+        self._dual_encoder = kwargs["dual_encoder"]
+
+    def post(self):
+        input_request = self.request.body
+        output = {'error_code': 0, 'error_message': ''}
+        if input_request is None:
+            output['error_code'] = 1
+            output['error_message'] = "Input is empty"
+            self.write(json.dumps(output))
+            return
+
+        try:
+            input_data = json.loads(input_request)
+        except:
+            output['error_code'] = 2
+            output['error_message'] = "Load input request error"
+            self.write(json.dumps(output))
+            return
+
+        if input_data['step'] == 1:
+            # encode query
+            query = input_data['query']
+            start = time.time()
+            q_embs = self._dual_encoder.encode_query(query=query)
+            q_embs = [x.tolist() for x in list(q_embs)]
+            end = time.time()
+            print('encode query: {}s'.format(end - start))
+            output['result'] = q_embs
+
+        elif input_data['step'] == 3:
+            title = input_data['title']
+            para = input_data['para']
+            start = time.time()
+            q_embs = self._dual_encoder.encode_para(title=[title], para=[para])
+            q_embs = [x.tolist() for x in list(q_embs)]
+            end = time.time()
+            print('encode para: {}s'.format(end - start))
+            output['result'] = q_embs
+
+        result_str = json.dumps(output, ensure_ascii=False)
+        self.write(result_str)
+
+
+def create_rocketqa_app(sub_address, rocketqa_server):
+    de_conf = {
+        "model": 'zh_dureader_de_v2',
+        "use_cuda": False,
+        "device_id": 0,
+        "batch_size": 32
+    }
+    dual_encoder = rocketqa.load_model(**de_conf)
+    return web.Application([(sub_address, rocketqa_server, dict(dual_encoder=dual_encoder))])
+
+
+if __name__ == "__main__":
+    sub_address = r'/rocketqa'
+    port = 25565
+    app = create_rocketqa_app(sub_address, RocketQAServer)
+    app.listen(port)
+    ioloop.IOLoop.current().start()
