000690aedec76384f86af849e92be5799f2097cd
w1049 <w10493wang@163.com>
2022-08-09 18:56:26 +0800

    guardian when user is None

--- app/guardian.py
+++ app/guardian.py
@@ -2,36 +2,48 @@ from .models import User, QuestionSet, Question, EnumRole
 
 
 def can_create_question(user: User):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     return True
 
 
 def can_get_question(user: User, question: Question):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     return question.created_by == user or can_get_question_set(user, question.belongs.first())
 
 
 def can_modify_question(user: User, question: Question):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     return question.created_by == user or can_modify_question_set(user, question.belongs.first())
 
 
 def can_delete_question(user: User, question: Question):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     return question.created_by == user or can_modify_question_set(user, question.belongs.first())
 
 
 def can_create_question_set(user: User):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     return True
 
 
 def can_get_question_set(user: User, question_set: QuestionSet):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     if user in question_set.maintainer:
@@ -40,6 +52,8 @@ def can_get_question_set(user: User, question_set: QuestionSet):
 
 
 def can_modify_question_set(user: User, question_set: QuestionSet):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     if user in question_set.maintainer:
@@ -48,6 +62,8 @@ def can_modify_question_set(user: User, question_set: QuestionSet):
 
 
 def can_delete_question_set(user: User, question_set: QuestionSet):
+    if not user:
+        return False
     if user.role == EnumRole.admin:
         return True
     if user in question_set.maintainer:
